7dc900a9fabe16574694b8c6c7ef0b37ba4d6168
gh-dk <deepak.kanojiya@neosoftmail.com>
2024-10-01 20:44:11 +0530

    added Env file for test in js form

---
+++ server/env.js
@@ -0,0 +1,4 @@
+PORT=3000
+MONGODB="mongodb+srv://deepakkanojiya:LdYVKZBgPsYrsC63@cluster0.78akxjh.mongodb.net/taskManagement?retryWrites=true&w=majority&appName=Cluster0"
+JWT_REFRESH_TOKEN="qwertyuiop"
+JWT_ACCESS_TOKEN="qwaszxrefdvc"
