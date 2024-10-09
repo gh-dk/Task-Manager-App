e345a1c3ece861879ba2484147280e92439c58ac
gh-dk <deepak.kanojiya@neosoftmail.com>
2024-10-09 16:09:43 +0530

    feat : added lazy loading and changed the build file budget size

---
+++ client/src/app/components/manage-task/manage-task.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManageTaskRoutingModule } from './manage-task-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManageTaskRoutingModule
+  ]
+})
+export class ManageTaskModule { }
--- client/src/routers/router.ts
+++ client/src/routers/router.ts
@@ -2,10 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../app/views/login/login.component';
 import { RegisterComponent } from '../app/views/register/register.component';
 import { DashboardComponent } from '../app/views/dashboard/dashboard.component';
-import { MainComponent } from '../app/components/main/main.component';
-import { ManageTaskComponent } from '../app/components/manage-task/manage-task.component';
 import { authGuard } from '../app/guards/auth.guard';
-import { ProfileComponent } from '../app/components/profile/profile.component';
 
 const router: Routes = [
   {
@@ -29,15 +26,25 @@ const router: Routes = [
     children: [
       {
         path: '',
-        component: MainComponent,
+        // component: MainComponent,
+        loadChildren: () =>
+          import('../app/components/main/main.module').then(
+            (m) => m.MainModule
+          ),
       },
       {
         path: 'task',
-        component: ManageTaskComponent,
+        loadChildren: () =>
+          import('../app/components/manage-task/manage-task.module').then(
+            (m) => m.ManageTaskModule
+          ),
       },
       {
         path: 'profile',
-        component: ProfileComponent,
+        loadChildren: () =>
+          import('../app/components/profile/profile.module').then(
+            (m) => m.ProfileModule
+          ),
       },
     ],
   },
